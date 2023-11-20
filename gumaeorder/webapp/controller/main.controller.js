fc7543d82b2d3e756261a72c022a3a7ac7b22841
kidofhavoc <woghd9802@gmail.com>
2023-11-20 11:33:28 +0900

    구매오더 콤보박스 생성

--- gumaeorder/webapp/controller/main.controller.js
+++ gumaeorder/webapp/controller/main.controller.js
@@ -1,15 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,JSONModel) {
         "use strict";
 
         return Controller.extend("gumaeorder.gumaeorder.controller.main", {
             onInit: function () {
-
+                var oData = {list : [
+                    {name : '김건진', test :'2'},
+                    {name : '신예솔', test :'9'},
+                    {name : '안은솔', test :'10'},
+                    {name : '안재홍', test :'11'},
+                    {name : '임지윤', test :'15'},
+                    {name : '정한식', test :'17'},
+                    {name : '하소명', test :'20'},
+                ]}
+                var oModel = new JSONModel(oData)
+                this.getView().setModel(oModel, 'test')
             }
         });
     });
