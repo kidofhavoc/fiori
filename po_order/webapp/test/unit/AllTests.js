ccd774adf448cfa160f774f06cafdfffaf37de0e
kidofhavoc <woghd9802@gmail.com>
2023-11-21 14:53:15 +0900

    odata 연결 프로그램 생성

---
+++ po_order/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"po_order/po_order/test/unit/controller/main.controller"
+], function () {
+	"use strict";
+});
---
+++ po_order/webapp/test/unit/controller/main.controller.js
@@ -0,0 +1,16 @@
+/*global QUnit*/
+
+sap.ui.define([
+	"po_order/po_order/controller/main.controller"
+], function (Controller) {
+	"use strict";
+
+	QUnit.module("main Controller");
+
+	QUnit.test("I should test the main controller", function (assert) {
+		var oAppController = new Controller();
+		oAppController.onInit();
+		assert.ok(oAppController);
+	});
+
+});
